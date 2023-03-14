bb0d56e0992ca63cc0b6bb8b1105af387ed3b6c5
cinthia <cinthiaduarte013@gmail.com>
2023-03-13 22:07:39 -0600

    primera clase

---
+++ app/src/main/java/com/example/myconvertidor/convertidor/multiplica.kt
@@ -0,0 +1,8 @@
+package com.example.myconvertidor.convertidor
+
+public class multiplica() {
+    var pnumero:Int=0
+    var snumero:Int=0
+    fun multiplica(pnumero:Int, snumero:Int):Int
+    { return (pnumero*snumero) }
+}
